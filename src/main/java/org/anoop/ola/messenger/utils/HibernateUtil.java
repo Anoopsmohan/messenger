ff48b951d3f1052c0d3e75f740b9e23f85d54a07
Anoopsmohan <anoopmhn2009@gmail.com>
2016-02-05 19:28:28 +0530

    Removed logs

--- src/main/java/org/anoop/ola/messenger/utils/HibernateUtil.java
+++ src/main/java/org/anoop/ola/messenger/utils/HibernateUtil.java
@@ -8,12 +8,10 @@ public class HibernateUtil {
 	
 	static {
 		try {
-				System.out.println("*********************************************");
 				// Create the SessionFactory from standard (hibernate.cfg.xml)
 				// config file.
 				sessionFactory = new Configuration().configure().buildSessionFactory();
 			} catch (Throwable ex) {
-				System.out.println("********************************************&&&&&&&&&&&&&&&&&&&&");
 				// Log the exception.
 				System.err.println("Initial SessionFactory creation failed." + ex);
 				throw new ExceptionInInitializerError(ex);
